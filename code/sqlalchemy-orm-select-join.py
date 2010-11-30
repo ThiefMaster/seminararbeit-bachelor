2f0cc4e5d7c8e34bcca3c4f98379c43367d51137
Adrian <adrian@planetcoding.net>
2010-11-30 22:55:29 +0100

    Mention iterator syntax; use it instead of .all()

--- code/sqlalchemy-orm-select-join.py
+++ code/sqlalchemy-orm-select-join.py
@@ -2,6 +2,6 @@ q = session.query(Book)
 q = q.join(Book.tags)
 q = q.options(joinedload(Book.tags))
 q = q.filter(Tag.tag == 'jquery')
-for book in q.all():
+for book in q:
     print book
     print book.tags
